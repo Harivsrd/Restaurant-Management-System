cbb1903b081ba714cfa7f11676038830787a2158
Harivsrd <harivsrdesineedi@gmail.com>
2026-05-21 13:24:21 +0530

    setup: initailize react frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import "../styles/Navbar.css";
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>
+                Restaurant Management System
+            </h1>
+        </nav>
+    )
+}
+
+export default Navbar;
