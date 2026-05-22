fe7615d25d464cebb6882f07dfbf7b7639623025
Harivsrd <harivsrdesineedi@gmail.com>
2026-05-23 00:54:41 +0530

    feat: create order history api

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
 
-from .views import create_order
+from .views import (create_order, order_history)
 
 urlpatterns = [
     path('create/', create_order),
+    path('history/', create_order)
 ]
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -22,3 +22,12 @@ def create_order(request):
     serializer = OrderSerializer(order)
     
     return Response(serializer.data)
+
+@api_view(['GET'])
+def order_history(request):
+    orders = Order.objects.all().order_by('-created_at')
+    serializer = OrderSerializer(
+        orders, many = True
+    )
+    
+    return Response(serializer.data)
