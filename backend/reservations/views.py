cddfd1c526d92bc16075bcb4559add4e8ad002e3
Harivsrd <harivsrdesineedi@gmail.com>
2026-05-24 00:58:00 +0530

    feat: add reservation validation logic

--- backend/reservations/views.py
+++ backend/reservations/views.py
@@ -7,14 +7,26 @@ from .models import Reservation
 from .serializers import ReservationSerializer
 
 from django.contrib.auth.models import User 
+from datetime import date 
 
 @api_view(['POST'])
 def create_reservation(request):
+    reservation_date = request.data.get("reservation_date")
+    guests = int(request.data.get("guests"))
+    if guests <= 0:
+        return Response({"error":"Guests must be greater than 0"}, status=400)
+    
+    if reservation_date < str(date.today()):
+        return Response({"error":"Connot book past dates"}, status=400)
+    
+    
+    
     user = User.objects.first()
     reservation = Reservation.objects.create(
-        user=user, reservation_date = request.data.get("reservation_date"),
+        user=user, 
+        reservation_date = reservation_date,
         reservation_time = request.data.get("reservation_time"),
-        guests = request.data.get("guests")
+        guests = guests
     )
     
     serializer = ReservationSerializer(reservation)
