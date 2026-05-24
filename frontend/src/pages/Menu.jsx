2fee8fc1717d34adf5731b9181b92c7f08574bb4
Harivsrd <harivsrdesineedi@gmail.com>
2026-05-25 02:26:31 +0530

    feat: implement search and category filtering

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -7,6 +7,10 @@ function Menu({ cartItems, setCartItems }) {
 
     const [menuItems, setMenuItems] = useState([]);
     
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const [selectedCategory, setSelectedCategory] = useState("All");
+
     const fetchMenu = async () => {
         try {
             const response = await API.get("menu/");
@@ -17,6 +21,17 @@ function Menu({ cartItems, setCartItems }) {
             console.log(error);
         }
     };
+
+    const filteredItems = menuItems.filter((item) => {
+        const matchesSearch = item.name.toLowerCase().includes(searchTerm.toLowerCase());
+
+        const matchesCategory = selectedCategory === "All" || 
+        item.category === selectedCategory;
+
+        return (
+            matchesSearch && matchesCategory
+        );
+    });
 
     useEffect(() => {
         fetchMenu();
@@ -46,10 +61,27 @@ function Menu({ cartItems, setCartItems }) {
 
     return (
         <div className="menu-container">
+
+            <div className="menu-controls">
+
+                <input type="text" placeholder="Search food..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+
+                <select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} >
+
+                    <option value="All">All</option>
+                    <option value="Biryani">Biryani</option>
+                    <option value="Pizza">Pizza</option>
+                    <option value="Drinks">Drinks</option>
+                    <option value="Desserts">Desserts</option>
+
+                </select>
+
+            </div>
+
             <h1>Restaurant Menu</h1>
 
             <div className="menu-grid">
-                {menuItems.map((item) => (
+                {filteredItems.map((item) => (
                     <MenuCard key={item.id} item={item} addToCart={addToCart} />
                 ))}
             </div>
