5563b63f85723f49e28f8d33c83153960ad4b8eb
Harivsrd <harivsrdesineedi@gmail.com>
2026-05-25 22:28:48 +0530

    feat: create favorites backend system

--- backend/menu/urls.py
+++ backend/menu/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from .views import menu_list 
+from .views import (menu_list , toggle_favorite , favorite_items )
 
 urlpatterns = [
     path('', menu_list),
+    path('favorites/', favorite_items),
+    path('favorites/<int:item_id>/',toggle_favorite),
 ]
--- backend/menu/views.py
+++ backend/menu/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import ( api_view , permission_classes )
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
 from .models import MenuItem
 from .serializer import MenuItemSerializer
@@ -15,3 +16,32 @@ def menu_list(request):
     serializer = MenuItemSerializer(result_page, many=True)
     return paginator.get_paginated_response(serializer.data)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+
+def toggle_favorite(request, item_id):
+    user = request.user
+    item = MenuItem.objects.get(id=item_id)
+    if user in item.favorites.all():
+        item.favorites.remove(user)
+        
+        return Response({
+            "message": "Removed from favorites"
+        })
+    else:
+        item.favorites.add(user)
+        
+        return Response({
+            "message": "Added to favorites"
+        })
+        
+        
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+
+def favorite_items(request):
+    user = request.user 
+    items = user.favorite_item.all()
+    serializer = MenuItemSerializer(items, many=True)
+    
+    return Response(serializer.data)
