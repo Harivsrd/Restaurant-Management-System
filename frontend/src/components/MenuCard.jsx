5ebbc4e421007ccc57dc9a8c029d00b057f53ddf
Harivsrd <harivsrdesineedi@gmail.com>
2026-05-28 01:01:04 +0530

    feat: implement frontend review and rating ui

--- frontend/src/components/MenuCard.jsx
+++ frontend/src/components/MenuCard.jsx
@@ -1,8 +1,19 @@
 import "../styles/menucard.css";
 import API from "../services/api";
 import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
 function MenuCard({ item, addToCart }) {
+
+    const [ rating, setRating ] = useState("");
+
+    const [comment , setComment] = useState("");
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetchReviews();
+    },[])
 
     const handleFavorite = async () => {
 
@@ -18,6 +29,27 @@ function MenuCard({ item, addToCart }) {
     }
     };
 
+    const submitReview = async () => {
+        try {
+            await API.post(`menu/review/${item.id}/`,{rating,comment,});
+            toast.success("Review Added");
+        }
+        catch(error) {
+            toast.error("Review Failed");
+        }
+    }
+
+    const fetchReviews = async () => {
+        try {
+            const response = await API.get(`menu/reviews/${item.id}/`);
+            setReviews(response.data);
+        }
+        catch(error) {
+            console.log(error);
+        }
+    };
+
+
     return (
         <div className="menu-card">
             <img 
@@ -31,6 +63,23 @@ function MenuCard({ item, addToCart }) {
             <button onClick={handleFavorite}>
             ❤️ Favorite
             </button>
+            <div className="review-section">
+                <input type="number" placeholder="Rating (1-5)" value={rating} onChange={(e) => setRating(e.target.value)} />
+                <textarea placeholder="Write review" value={comment} onChange={(e) => setComment(e.target.value)} />
+                <button onClick={submitReview}> Submit Review</button>
+            </div>
+            <div className="reviews-list">
+                {
+                    reviews.map((review) => (
+                        <div key={review.id}>
+                            <h4>{review.username}</h4>
+                            <p>⭐ {review.rating}</p>
+                            <p>{review.rating}</p>
+                            <p>{review.comment}</p>
+                        </div>
+                    ))
+                }
+            </div>
             <button onClick={() => addToCart(item)}>Add to Cart</button>
         </div>
     );
